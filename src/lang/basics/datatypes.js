f4eb936255c73bf20136c49e4f04baee2f5a6844
Abhishek Saseendran <abhisheksaseendran@Abhisheks-MacBook-Pro.local>
2021-05-17 01:53:53 +0530

    Added Datatypes.

---
+++ src/lang/basics/datatypes.js
@@ -0,0 +1,23 @@
+console.log("========== DataTypes ==========");
+
+var name = "Bob"; // String
+console.log("01. DataType =>", typeof name);
+
+var age = 10; // Number
+console.log("02. DataType =>", typeof age);
+
+var languages = ['English','Hindi']; // Object
+console.log("03. DataType =>", typeof languages);
+
+var bool = true; // Boolean
+console.log("04. DataType =>", typeof bool);
+
+var greet = function() { console.log("Hello Bob!") } // function
+console.log("05. DataType =>", typeof greet);
+
+var city; // undefined
+console.log("06. DataType =>", typeof city); 
+
+var address = null; // Object
+console.log("07. DataType =>", typeof address);
+
