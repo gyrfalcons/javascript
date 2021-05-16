5620e3d626a3b89b04c92391b927749f9e250fb1
Abhishek Saseendran <abhisheksaseendran@Abhisheks-MacBook-Pro.local>
2021-05-17 02:10:20 +0530

    Added template literal

---
+++ src/lang/ES6/template-literal.js
@@ -0,0 +1,9 @@
+let person = {
+    firstName : 'Tom',
+    lastName : 'Cruise',
+    age : 55
+};
+
+let userInfo = `Hi, My name is ${person.firstName} ${person.lastName}. I am ${person.age} years old.`;
+
+console.log(userInfo);
